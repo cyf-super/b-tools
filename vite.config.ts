822daa1d9cc42a9708c1e504586b93e79d8a7e6d
cyf-super <1753760980@qq.com>
2026-05-10 23:02:50 +0800

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/b-tools',
+  // base: '/b-tools',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
